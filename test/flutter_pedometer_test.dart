b84dc047b708c7d50da7963b43557e2e46b3a7ff
Wisdom Ekeh <ekeh.wisdom@gmail.com>
2022-01-14 11:13:06 +0100

    basic setup

---
+++ test/flutter_pedometer_test.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/services.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:flutter_pedometer/flutter_pedometer.dart';
+
+void main() {
+  const MethodChannel channel = MethodChannel('flutter_pedometer');
+
+  TestWidgetsFlutterBinding.ensureInitialized();
+
+  setUp(() {
+    channel.setMockMethodCallHandler((MethodCall methodCall) async {
+      return '42';
+    });
+  });
+
+  tearDown(() {
+    channel.setMockMethodCallHandler(null);
+  });
+
+  test('getPlatformVersion', () async {
+    expect(await FlutterPedometer.platformVersion, '42');
+  });
+}
